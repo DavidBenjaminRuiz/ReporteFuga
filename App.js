e1789ad8eabfe2e86417f521d5852c79a6ef0f20
David Ruiz <david.ruiz7454@alumnos.udg.mx>
2021-06-15 17:26:32 -0500

    Campos de ingreso de datos

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, Button } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import * as ImagePicker from 'expo-image-picker'
 
 import  aguaImg from './assets/agua.png';
+import  noFotoImg from './assets/no-photos.png';
 
 
 //////////////////Login/////////////////////
@@ -33,14 +35,61 @@ import  aguaImg from './assets/agua.png';
 }
 ///////////////////Reporte//////////////////////////
 
-  function Reporte(){
+function Reporte(){
+  const [direccionFuga, onChangeDireccion]= React.useState("")
+  const [comentarios, onChangeComentarios]= React.useState("")
+  const [selectedImage, setSelectedImage]= React.useState(null)
+
+  let openImagePicker= async () =>{
+
+    let permisos= await ImagePicker.requestMediaLibraryPermissionsAsync()
+    if (permisos.granted === false){
+      alert("Permisos requeridos") 
+      return;
+    }
+    let imgPickeada= await ImagePicker.launchImageLibraryAsync() 
+    if (imgPickeada.cancelled === true){
+      return;
+    }
+    setSelectedImage({localUri: imgPickeada.uri});
+  }  
+
 return (
   <View style={styles.containerReporte}>
   <Text style={styles.tituloVentana2}> Crea tu Reporte</Text>
 
+  <TextInput 
+    style={styles.inputs}
+    placeholder= "Direccion de la fuga"
+    onChangeText={onChangeDireccion}
+    value={direccionFuga}
+
+  />
+  <TextInput 
+    style={styles.inputs}
+    placeholder= "Observaciones adicionales"
+    onChangeText={onChangeComentarios}
+    value={comentarios }
+    keyboardType= 'ascii-capable'
+    multiline
+     
+
+  />
+
+  <Image
+    style={styles.fotoFuga}
+    source={{
+      uri:
+      selectedImage !== null ? selectedImage.localUri : "/assets/no-photos.png",
+    }}
+  />
+  <Button
+    title="Cargar Foto de la Fuga"
+    onPress={openImagePicker}
+  />
 
   <TouchableOpacity style={styles.botonEnviar} 
-    onPress={() => console.log("works")}>
+    onPress={() => console.log(direccionFuga, comentarios)}>
     <Text style={styles.tituloBtn}> Enviar Reporte</Text>
   </TouchableOpacity>
   </View>
@@ -57,12 +106,12 @@ export default function App() {
       <Stack.Navigator initialRouteName="INCO 2021">
         <Stack.Screen name="INCO 2021" component={Login} options={{
           headerStyle: {
-            backgroundColor: '#A2D5F2',
+            backgroundColor: '#82d5f2',
           }
         }}/>
         <Stack.Screen name="Reporte" component={Reporte}   options={{
           headerStyle: {
-            backgroundColor: '#A2D5F2',
+            backgroundColor: '#82d5f2',
           }
         }} />
       </Stack.Navigator>
@@ -118,6 +167,19 @@ const styles = StyleSheet.create({
     padding: 10,
     marginTop: 100,
     borderRadius: 10
+  },
+  inputs: {
+    height:50,
+    marginTop: 40,
+    borderWidth: 1.5,
+    width: 250,
+    padding: 10,
+    borderRadius: 5
+  },
+  fotoFuga: {
+    width:150,
+    height:150,
+    marginTop: 20
   }
 });
 
